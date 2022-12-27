0532d2c04418c482360e05fddc08177e767c2e0a
jshvaron <jshva@icloud.com>
2022-12-27 16:13:56 -0600

    edits border colors and shadows

--- assets/script.js
+++ assets/script.js
@@ -3,13 +3,13 @@ const startQuiz = document.getElementById("startbtn");
 const countDown = document.getElementById("countdown"); 
 let countStart = 10;
 
-//Starts reduction of timer on click
+//Starts reduction of timer on click, alerts end of timer and then reloads page
 startQuiz.addEventListener("click", function(){
     setInterval(function(){
        countStart--;
        countDown.textContent = "Timer: " + countStart;
        if(countStart === -1){
-        alert("Your time is up, better luck next time!")
+        alert("Your time is up, Click 'ok' to restart.")
         window.location.reload();
        };
        
